2b1808a440c0a4ca428a84388114bd1816507147
ahmed ifhaam <ifhaam.kr@gmail.com>
2018-01-11 15:58:33 +0530

    initial commit of dhis2-android-new-sdk we use for tibet

---
+++ ui/build/generated/source/buildConfig/debug/org/hisp/dhis/client/sdk/ui/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.hisp.dhis.client.sdk.ui;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.hisp.dhis.client.sdk.ui";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 45;
+  public static final String VERSION_NAME = "0.3.28-2.24";
+}
---
+++ ui/build/generated/source/buildConfig/release/org/hisp/dhis/client/sdk/ui/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.hisp.dhis.client.sdk.ui;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "org.hisp.dhis.client.sdk.ui";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 45;
+  public static final String VERSION_NAME = "0.3.28-2.24";
+}
